184cb5c76a6a2f04bba34449e7a14905b72c0626
MovchanNatalia <Movchan.n2106@Gmail.com>
2023-07-30 13:12:19 +0300

    add task

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # node-js-hw-1-CLI
+
+https://ibb.co/Hh03Rp0
--- contacts.js
+++ contacts.js
@@ -1,11 +1,12 @@
 const fs = require("fs").promises;
 const path = require("path");
+const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
+    return JSON.parse(data);
 };
 
 const getContactById = async (contactId) => {
@@ -35,6 +36,8 @@ const addContact = async (data) => {
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
 };
+
+
 
 module.exports = {
   listContacts,
